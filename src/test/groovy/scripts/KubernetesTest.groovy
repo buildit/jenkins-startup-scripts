f34ad1e9c1b433dc4c006beb10de89b7f0796eae
Dermot Burke <dermotmburke@gmail.com>
2018-04-16 14:49:51 +0100

    Upgrade k8s plugin

--- src/test/groovy/scripts/KubernetesTest.groovy
+++ src/test/groovy/scripts/KubernetesTest.groovy
@@ -19,8 +19,10 @@ class KubernetesTest extends StartupTest {
     @Test
     @LocalData
     @ZipTestFiles(files = ["jenkins.config"])
-    @WithPlugin(["kubernetes-1.1.4.hpi", "workflow-step-api-2.14.hpi", "credentials-2.1.16.hpi", "durable-task-1.15.hpi", "variant-1.1.hpi", "structs-1.10.hpi"])
+    @WithPlugin(["kubernetes-1.5.2.hpi", "workflow-step-api-2.14.hpi", "credentials-2.1.16.hpi", "durable-task-1.15.hpi", "variant-1.1.hpi", "structs-1.10.hpi",
+            "kubernetes-credentials-0.3.0.hpi", "plain-credentials-1.4.hpi"])
     void shouldSetUpHostDetailsFromConfig() {
+        sleep(1000000)
         def clouds = jenkinsRule.instance.clouds
         def cloud = clouds[0]
         assertThat(cloud.name as String, equalTo('My Cloud'))

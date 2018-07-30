dcb5f082b73cbebebdbc170a647406905a7987d3
Tim Jacomb <tim.jacomb@hmcts.net>
2018-07-30 14:31:02 +0100

    Update for new version of the plugin

--- scripts/scripts/sonarqube.groovy
+++ scripts/scripts/sonarqube.groovy
@@ -6,8 +6,6 @@ import jenkins.model.Jenkins
 import java.util.logging.Level
 import java.util.logging.Logger
 
-import static hudson.plugins.sonar.utils.SQServerVersions.SQ_5_3_OR_HIGHER
-
 def descriptor = Jenkins.getInstance().getDescriptorByType(SonarGlobalConfiguration.class)
 def logger = Logger.getLogger("sonarqube.groovy")
 
@@ -24,7 +22,7 @@ config?.each { instance ->
             instance.name as String,
             instance.serverUrl as String, 
             instance.serverAuthenticationToken as String,
-            SQ_5_3_OR_HIGHER, 
+            null,
             "-XX", 
             new TriggersConfig(),
             "sonar.organization=bleugh"

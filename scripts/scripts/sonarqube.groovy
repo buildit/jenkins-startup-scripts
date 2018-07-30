544ea7de68eb14d187bd88c39a5ca157d8bef178
Tim Jacomb <tim.jacomb@hmcts.net>
2018-07-30 13:56:01 +0100

    Remove backwards compatibility for sonarqube

--- scripts/scripts/sonarqube.groovy
+++ scripts/scripts/sonarqube.groovy
@@ -6,7 +6,6 @@ import jenkins.model.Jenkins
 import java.util.logging.Level
 import java.util.logging.Logger
 
-import static hudson.plugins.sonar.utils.SQServerVersions.SQ_5_1_OR_LOWER
 import static hudson.plugins.sonar.utils.SQServerVersions.SQ_5_3_OR_HIGHER
 
 def descriptor = Jenkins.getInstance().getDescriptorByType(SonarGlobalConfiguration.class)
@@ -20,15 +19,22 @@ config?.each { instance ->
 
     println("Adding sonar installer with name ${instance.name} and url ${instance.serverUrl}")
     logger.log(Level.INFO, "Adding sonar installer with name ${instance.name} and url ${instance.serverUrl}")
-    String version = SQ_5_1_OR_LOWER //default to 5.1 or lower to maintain backwards compatibility to when this was hardcoded to that
-    if (instance.version.equals("5.3 or higher")) {
-        version = SQ_5_3_OR_HIGHER
-    }
-    def installation = new SonarInstallation(instance.name as String,
-            instance.serverUrl as String, version, instance.serverAuthenticationToken as String,
-            instance.databaseUrl as String, instance.databaseLogin as String, instance.databasePassword as String,
-            "", "", new TriggersConfig(),
-            instance.sonarLogin as String, instance.sonarPassword as String, "")
+
+    def installation = new SonarInstallation(
+            instance.name as String,
+            instance.serverUrl as String, 
+            SQ_5_3_OR_HIGHER, 
+            instance.serverAuthenticationToken as String,
+            instance.databaseUrl as String, 
+            instance.databaseLogin as String, 
+            instance.databasePassword as String,
+            "", 
+            "", 
+            new TriggersConfig(),
+            instance.sonarLogin as String, 
+            instance.sonarPassword as String, 
+            ""
+        )
 
     installations.add(installation)
 
